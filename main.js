3c1d7ca6043523588c36101f192801c0ec9dd5f7
salagata <122649005+salagata@users.noreply.github.com>
2025-11-26 00:41:21 -0500

    Set up libraries

---
+++ main.js
@@ -0,0 +1,24 @@
+const pathToFfmpeg = require('ffmpeg-static');
+const path = require("path");
+
+const { app, BrowserWindow } = require('electron');
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
+
+  win.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
